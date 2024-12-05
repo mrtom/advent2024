dafdb2e36ed2abeb73b60c8a1c0f7e7e92c3cbee
Tom Elliott <tom@letsdothis.com>
2024-12-05 16:17:12 +0000

    Day 5, Part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,4 @@
-use std::ops::Index;
+use std::result::Result;
 
 use regex::Regex;
 
@@ -7,8 +7,8 @@ use crate::AOCDay;
 pub struct Day5 {}
 
 struct Rule {
-  pageBefore: i32,
-  pageAfter: i32,
+  page_before: i32,
+  page_after: i32,
 }
 
 type Update = Vec<i32>;
@@ -34,14 +34,13 @@ fn parse_input(input: &[String]) -> Input {
       let page_before = capture.get(1).map(|m| m.as_str().parse::<i32>());
       let page_after = capture.get(2).map(|m| m.as_str().parse::<i32>());
       if let (Some(Ok(page_before)), Some(Ok(page_after))) = (page_before, page_after) {
-        rules.push(Rule { pageBefore: page_before, pageAfter: page_after });
+        rules.push(Rule { page_before, page_after });
       }
     } else {
-      let update = update_re.captures_iter(line).map(|capture| {
+      let update = update_re.captures_iter(line).filter_map(|capture| {
         capture.get(1).map(|m| m.as_str().parse::<i32>())
       })
-      .filter_map(|x| x)
-      .filter_map(|x| x.ok())
+      .filter_map(Result::ok)
       .collect();
       updates.push(update);
     }
@@ -50,20 +49,45 @@ fn parse_input(input: &[String]) -> Input {
   Input { rules, updates }
 }
 
+fn does_update_follow_rule(update: &Update, rule: &Rule) -> bool {
+  let before = update.iter().position(|x| *x == rule.page_before);
+  let after = update.iter().position(|x| *x == rule.page_after);
+  match (before, after) {
+    (Some(before), Some(after)) => before < after,
+    (None, Some(_) | None) | (Some(_), None) => true,
+  }
+}
+
 fn is_update_correct(update: &Update, rules: &[Rule]) -> bool {
-  rules.iter().all(|rule| {
-    let before = update.iter().position(|x| *x == rule.pageBefore);
-    let after = update.iter().position(|x| *x == rule.pageAfter);
-    match (before, after) {
-      (Some(before), Some(after)) => before < after,
-      (None, Some(_) | None) | (Some(_), None) => true,
-    }
-  })
+  rules.iter().all(|rule| does_update_follow_rule(update, rule))
 }
 
 fn get_middle_value(update: &Update) -> i32 {
-  let middle_idx = ((update.len() as f64) / 2.0).floor() as usize;
-  update[middle_idx]
+  match update.get(update.len() / 2) {
+    Some(&middle) => middle,
+    None => panic!("Update is empty. This should be impossible."),
+  }
+}
+
+fn fix_update(update: &Update, rules: &[Rule]) -> Update {
+  let mut fixed_update = update.clone();
+  while !is_update_correct(&fixed_update, rules)  {
+    if let Some(next_broken_rule) = rules.iter().find(|rule| !does_update_follow_rule(&fixed_update, rule)) {
+      if let (Some(before_idx), Some(after_idx)) = (
+        fixed_update.iter().position(|x| *x == next_broken_rule.page_before),
+        fixed_update.iter().position(|x| *x == next_broken_rule.page_after)
+      ) {
+          let element = fixed_update.remove(before_idx);
+          fixed_update.insert(after_idx, element);
+        } else {
+          panic!("Broken rule not found in update");
+      }
+    } else {
+      panic!("No broken rule found");
+    }
+
+  }
+  fixed_update
 }
 
 impl AOCDay for Day5 {
@@ -90,7 +114,15 @@ impl AOCDay for Day5 {
   }
   
   fn solve_part2(&self, input: &[String]) -> String {
-    "".to_string()
+    let input = parse_input(input);
+    let bad_updates = input.updates.iter().filter(|update| !is_update_correct(update, &input.rules));
+
+    let result = bad_updates
+    .map(|update| fix_update(update, &input.rules)) 
+    .map(|update| get_middle_value(&update))
+    .sum::<i32>();
+    
+    result.to_string()
   }
 }
 
@@ -98,12 +130,6 @@ impl AOCDay for Day5 {
 mod tests {
   use super::*;
   use crate::utils::read_file;
-  
-  #[test]
-  fn test_something() {
-    let result = "";
-    assert_eq!(result, "");
-  }
   
   #[test]
   fn test_part_1() {
@@ -118,7 +144,7 @@ mod tests {
   fn test_part_2() {
     let day = Day5 {};
     assert_eq!(
-      "TODO",
+      "5770",
       day.solve_part2(&read_file("input/day5/part1.txt"))
     );
   }
