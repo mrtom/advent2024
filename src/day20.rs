df5342efa2c9468dff11700b5d54317fa47252fc
Tom Elliott <tom@letsdothis.com>
2024-12-20 22:23:08 +0000

    Day 20, part 2.

--- src/day20.rs
+++ src/day20.rs
@@ -1,43 +1,62 @@
-use std::collections::HashSet;
-use pathfinding::prelude::astar;
+use crate::utils::Point;
 use crate::{utils, AOCDay};
+use pathfinding::prelude::astar;
+use std::collections::HashSet;
 
 const PART_1_EXAMPLE: &str = "5";
-const PART_2_EXAMPLE: &str = "FAIL";
+const PART_2_EXAMPLE: &str = "285";
 
 type Map = Vec<Vec<char>>;
 
-#[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
-struct Point {
-  x: i32,
-  y: i32,
-}
-
 fn parse_input(input: &[String]) -> (Map, Point, Point) {
-  let mut map = input.iter().map(|line| line.chars().collect()).collect::<Vec<Vec<char>>>();
-  let start = map.iter().enumerate().find_map(|(y, row)| {
-    row.iter().enumerate().find_map(|(x, &c)| {
-      if c == 'S' {
-        Some((utils::usize_to_i32(x).unwrap(), utils::usize_to_i32(y).unwrap()))
-      } else {
-        None
-      }
+  let mut map = input
+    .iter()
+    .map(|line| line.chars().collect())
+    .collect::<Vec<Vec<char>>>();
+  let start = map
+    .iter()
+    .enumerate()
+    .find_map(|(y, row)| {
+      row.iter().enumerate().find_map(|(x, &c)| {
+        if c == 'S' {
+          Some((
+            utils::usize_to_i32(x).unwrap(),
+            utils::usize_to_i32(y).unwrap(),
+          ))
+        } else {
+          None
+        }
+      })
     })
-  }).expect("No start position found");
-  let end = map.iter().enumerate().find_map(|(y, row)| {
-    row.iter().enumerate().find_map(|(x, &c)| {
-      if c == 'E' {
-        Some((utils::usize_to_i32(x).unwrap(), utils::usize_to_i32(y).unwrap()))
-      } else {
-        None
-      }
+    .expect("No start position found");
+  let end = map
+    .iter()
+    .enumerate()
+    .find_map(|(y, row)| {
+      row.iter().enumerate().find_map(|(x, &c)| {
+        if c == 'E' {
+          Some((
+            utils::usize_to_i32(x).unwrap(),
+            utils::usize_to_i32(y).unwrap(),
+          ))
+        } else {
+          None
+        }
+      })
     })
-  }).expect("No end position found");
+    .expect("No end position found");
 
   map[utils::i32_to_usize_x(start.1)][utils::i32_to_usize_x(start.0)] = '.';
   map[utils::i32_to_usize_x(end.1)][utils::i32_to_usize_x(end.0)] = '.';
 
-  (map, Point { x: start.0, y: start.1 }, Point { x: end.0, y: end.1 })
+  (
+    map,
+    Point {
+      x: start.0,
+      y: start.1,
+    },
+    Point { x: end.0, y: end.1 },
+  )
 }
 
 #[allow(dead_code)]
@@ -52,51 +71,150 @@ fn is_test(input: &[Vec<char>]) -> bool {
   input.len() == 15
 }
 
-fn get_path_neighbours(map: &Map, location: Point) -> Vec<(Point, i32)> {
-  get_neighbours(map, location, '.')
-}
+fn get_neighbours(
+  map: &Map,
+  location: Point,
+  cheat_start: Option<Point>,
+  cheat_end: Option<Point>,
+  cheat_distance: Option<i32>,
+) -> Vec<(Point, i32)> {
+  // If we've ended up on a cheat start, the only neighbour is the end of the cheat_path
+  if let (Some(cheat_start), Some(cheat_end), Some(cheat_distance)) =
+    (cheat_start, cheat_end, cheat_distance)
+  {
+    if location == cheat_start {
+      return vec![(cheat_end, cheat_distance)];
+    }
+  }
 
-fn get_neighbours(map: &Map, location: Point, of_type: char) -> Vec<(Point, i32)> {
+  // Otherwise, we are on the track and move as normal
   let mut neighbours = vec![];
-  
   for (dx, dy) in &[(0, -1), (0, 1), (-1, 0), (1, 0)] {
     let new_pos = (location.x + dx, location.y + dy);
     let col_max = utils::usize_to_i32_x(map[0].len());
     let row_max = utils::usize_to_i32_x(map.len());
-    
-    if new_pos.1 >= 0 && new_pos.1 < row_max && 
-        new_pos.0 >= 0 && new_pos.0 < col_max && 
-        map[utils::i32_to_usize_x(new_pos.1)][utils::i32_to_usize_x(new_pos.0)] == of_type 
+
+    if new_pos.1 >= 0
+      && new_pos.1 < row_max
+      && new_pos.0 >= 0
+      && new_pos.0 < col_max
+      && map[utils::i32_to_usize_x(new_pos.1)][utils::i32_to_usize_x(new_pos.0)] == '.'
     {
-      neighbours.push((Point {
-        x: new_pos.0,
-        y: new_pos.1,
-      }, 1));
-    }
-  }
-  
-  neighbours
-}
-
-fn get_wall_pairs(map: &Map) -> HashSet<(Point, Point)> {
-  let wall_tiles = map.iter().enumerate().flat_map(|(row_idx, col)| {
-    col.iter().enumerate().filter(|(_, value)| **value == '#').map(move |(col_idx, _)| {
-      Point { x: utils::usize_to_i32_x(col_idx), y: utils::usize_to_i32_x(row_idx) }
-    })
-  }).collect::<Vec<Point>>();
-
-  let mut result = HashSet::new();
-
-  for tile in wall_tiles {
-    for neighbour in get_path_neighbours(map, tile) {
-      result.insert((
-        Point { x: tile.x, y: tile.y },
-        Point { x: neighbour.0.x, y: neighbour.0.y },
+      neighbours.push((
+        Point {
+          x: new_pos.0,
+          y: new_pos.1,
+        },
+        1,
       ));
     }
   }
 
+  neighbours
+}
+
+fn generate_cheat_paths_for_tile(
+  map: &Map,
+  cheat_start: Point,
+  path_length: usize,
+) -> HashSet<(Point, Point, i32)> {
+  // Take all the tiles in the map
+  map
+    .iter()
+    .enumerate()
+    .flat_map(|(row_idx, col)| {
+      col.iter().enumerate().map(move |(col_idx, _)| Point {
+        x: utils::usize_to_i32_x(col_idx),
+        y: utils::usize_to_i32_x(row_idx),
+      })
+    })
+    // Then filter only the track tiles
+    .filter(|cheat_end| {
+      map[utils::i32_to_usize_x(cheat_end.y)][utils::i32_to_usize_x(cheat_end.x)] == '.'
+    })
+    // get the manhatten distance from the cheat start to the end
+    // and filter the ones that are within the path length
+    .filter_map(|cheat_end| {
+      let distance = utils::get_manhattan_distance(cheat_start, cheat_end);
+      if distance <= utils::usize_to_i32_x(path_length) {
+        Some((cheat_start, cheat_end, distance))
+      } else {
+        None
+      }
+    })
+    .collect::<HashSet<(Point, Point, i32)>>()
+}
+
+fn get_cheat_paths(map: &Map, path_length: usize) -> HashSet<(Point, Point, i32)> {
+  let track_tiles = map
+    .iter()
+    .enumerate()
+    .flat_map(|(row_idx, col)| {
+      col
+        .iter()
+        .enumerate()
+        .filter(|(_, value)| **value == '.')
+        .map(move |(col_idx, _)| Point {
+          x: utils::usize_to_i32_x(col_idx),
+          y: utils::usize_to_i32_x(row_idx),
+        })
+    })
+    .collect::<Vec<Point>>();
+
+  let mut result = HashSet::new();
+  for tile in track_tiles {
+    let paths = generate_cheat_paths_for_tile(map, tile, path_length);
+    for (cheat_start, cheat_end, cheat_path_length) in paths {
+      result.insert((cheat_start, cheat_end, cheat_path_length));
+    }
+  }
+
   result
+}
+
+fn find_shortest_path(
+  map: &Map,
+  start: Point,
+  end: Point,
+  cheat_start: Option<Point>,
+  cheat_end: Option<Point>,
+  cheat_distance: Option<i32>,
+) -> Option<(Vec<Point>, i32)> {
+  astar(
+    &start,
+    |loc| get_neighbours(map, *loc, cheat_start, cheat_end, cheat_distance),
+    |loc| utils::get_manhattan_distance(end, *loc),
+    |loc| *loc == end,
+  )
+}
+
+fn count_cheat_paths(
+  map: &Map,
+  start: Point,
+  end: Point,
+  path_length: usize,
+  required_saving: i32,
+) -> usize {
+  let total_cost = find_shortest_path(map, start, end, None, None, None)
+    .expect("Could not find valid path through maze")
+    .1;
+
+  get_cheat_paths(map, path_length)
+    .iter()
+    .filter_map(|(cheat_start, cheat_end, cheat_distance)| {
+      let result = find_shortest_path(
+        map,
+        start,
+        end,
+        Some(*cheat_start),
+        Some(*cheat_end),
+        Some(*cheat_distance),
+      );
+
+      result.map(|(_, cost)| (*cheat_start, *cheat_end, cost, total_cost - cost))
+    })
+    .filter(|(_, _, cost, _)| *cost <= total_cost - required_saving)
+    .count()
 }
 
 pub struct Day20 {}
@@ -105,51 +223,31 @@ impl AOCDay for Day20 {
   fn name(&self) -> String {
     "day20".to_string()
   }
-  
+
   fn test_answer_part1(&self) -> String {
     PART_1_EXAMPLE.to_string()
   }
-  
+
   fn test_answer_part2(&self) -> String {
     PART_2_EXAMPLE.to_string()
   }
-  
+
   fn solve_part1(&self, input: &[String]) -> String {
     let (map, start, end) = parse_input(input);
-    let is_test_run = is_test(&map);
-    let required_saving = if is_test_run { 20 } else { 100 };
-    let mut big_cheat_count = 0;
+    let required_saving = if is_test(&map) { 20 } else { 100 };
 
-    let total_cost =  astar(
-      &start,
-      |loc| get_path_neighbours(&map, *loc),
-      |loc| utils::u32_to_i32(end.x.abs_diff(loc.x) + end.y.abs_diff(loc.y)).unwrap(),
-      |loc| *loc == end,
-    ).expect("Could not find valid path through maze").1;
+    let result = count_cheat_paths(&map, start, end, 2, required_saving);
 
-    let cheat_starts = get_wall_pairs(&map).iter().map(|pair| pair.0).collect::<HashSet<Point>>();
-    for cheat in cheat_starts {
-      let mut new_map = map.clone();
-      new_map[utils::i32_to_usize_x(cheat.y)][utils::i32_to_usize_x(cheat.x)] = '.';
-
-      if let Some((_, new_cost)) = astar(
-        &start,
-        |loc| get_path_neighbours(&new_map, *loc),
-        |loc| utils::u32_to_i32(end.x.abs_diff(loc.x) + end.y.abs_diff(loc.y)).unwrap(),
-        |loc| *loc == end,
-      ) {
-        if total_cost - new_cost >= required_saving {
-          big_cheat_count += 1;
-        }
-      }
-    }
-
-    big_cheat_count.to_string()
+    result.to_string()
   }
-  
+
   fn solve_part2(&self, input: &[String]) -> String {
-    let input = parse_input(input);
-    "Not implemented".to_string()
+    let (map, start, end) = parse_input(input);
+    let required_saving = if is_test(&map) { 50 } else { 100 };
+
+    let result = count_cheat_paths(&map, start, end, 20, required_saving);
+
+    result.to_string()
   }
 }
 
@@ -166,14 +264,11 @@ mod tests {
       day.solve_part1(&read_file("input/day20/test1.txt"))
     );
   }
-  
+
   #[test]
   fn test_part_1() {
     let day = Day20 {};
-    assert_eq!(
-      "1321",
-      day.solve_part1(&read_file("input/day20/part1.txt"))
-    );
+    assert_eq!("1321", day.solve_part1(&read_file("input/day20/part1.txt")));
   }
 
   #[test]
@@ -184,12 +279,12 @@ mod tests {
       day.solve_part2(&read_file("input/day20/test1.txt"))
     );
   }
-  
+
   #[test]
   fn test_part_2() {
     let day = Day20 {};
     assert_eq!(
-      "TODO",
+      "971737",
       day.solve_part2(&read_file("input/day20/part1.txt"))
     );
   }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::string::ToString;
 
 pub fn read_file(file: &str) -> Vec<String> {
@@ -10,7 +11,13 @@ pub fn i32_to_usize(value: i32) -> Option<usize> {
   value.try_into().ok()
 }
 pub fn i32_to_usize_x(value: i32) -> usize {
-  value.try_into().expect("Failed to convert i32 to usize")
+  let v = value.try_into();
+
+  if v.ok().is_none() {
+    println!("Failed to convert i32 to usize: {value}");
+  }
+
+  v.expect("Failed to convert i32 to usize")
 }
 
 pub fn i32_to_u32_x(value: i32) -> u32 {
@@ -21,13 +28,39 @@ pub fn usize_to_i32(value: usize) -> Option<i32> {
   value.try_into().ok()
 }
 pub fn usize_to_i32_x(value: usize) -> i32 {
- value.try_into().expect("Failed to convert usize to i32")
+  value.try_into().expect("Failed to convert usize to i32")
 }
 
 pub fn u32_to_i32(value: u32) -> Option<i32> {
   value.try_into().ok()
 }
+pub fn u32_to_i32_x(value: u32) -> i32 {
+  value.try_into().expect("Failed to convert u32 to i32")
+}
 
 pub fn isize_to_usize_x(value: isize) -> usize {
   value.try_into().expect("Failed to convert isize to usize")
+}
+
+// Mark - Points
+
+#[derive(Debug, Copy, Clone, Eq, Hash, PartialEq)]
+pub struct Point {
+  pub x: i32,
+  pub y: i32,
+}
+
+impl fmt::Display for Point {
+  // This trait requires `fmt` with this exact signature.
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    // Write strictly the first element into the supplied output
+    // stream: `f`. Returns `fmt::Result` which indicates whether the
+    // operation succeeded or failed. Note that `write!` uses syntax which
+    // is very similar to `println!`.
+    write!(f, "{},{}", self.x, self.y)
+  }
+}
+
+pub fn get_manhattan_distance(a: Point, b: Point) -> i32 {
+  u32_to_i32_x(a.x.abs_diff(b.x) + a.y.abs_diff(b.y))
 }
