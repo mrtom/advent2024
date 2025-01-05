b4593db8ed0e284049d16b3a8bb55f9393ee7b69
Tom Elliott <tom.elliott@faculty.ai>
2025-01-05 19:34:51 +0000

    Use Rayon to run in parallel, and change main to allow only running the solution code, rather than both solution and test code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rayon::iter::IntoParallelRefIterator;
+use rayon::prelude::*;
+
 mod day1;
 mod day10;
 mod day11;
@@ -25,6 +28,7 @@ mod day8;
 mod day9;
 mod utils;
 
+use std::env;
 use std::path::Path;
 
 use day1::Day1;
@@ -53,7 +57,7 @@ use day7::Day7;
 use day8::Day8;
 use day9::Day9;
 
-pub trait AOCDay {
+pub trait AOCDay: Sync {
   fn name(&self) -> String;
   fn test_answer_part1(&self) -> String;
   fn test_answer_part2(&self) -> String;
@@ -62,56 +66,86 @@ pub trait AOCDay {
   fn solve_part2(&self, input: &[String]) -> String;
 }
 
-fn run_day(day: &impl AOCDay) {
+fn get_filenames(day_name: &str) -> (String, String, String, String) {
+  let test_data_1_name = &format!("input/{day_name}/test1.txt");
+  let input_1_name = &format!("input/{day_name}/part1.txt");
+  let test_data_2_name = &format!("input/{day_name}/test2.txt");
+  let input_2_name = &format!("input/{day_name}/part2.txt");
+
+  let test_data_name = if Path::new(test_data_2_name).exists() {
+    test_data_2_name
+  } else {
+    test_data_1_name
+  };
+  let input_data_name = if Path::new(input_2_name).exists() {
+    input_2_name
+  } else {
+    input_1_name
+  };
+
+  (
+    test_data_1_name.to_string(),
+    input_1_name.to_string(),
+    test_data_name.to_string(),
+    input_data_name.to_string(),
+  )
+}
+
+fn run_day(day: &dyn AOCDay) {
   println!("Running {}", day.name());
+  let (_, input_1_name, _, input_2_name) = get_filenames(&day.name());
+  let input_data1 = utils::read_file(&input_1_name);
+  let input_data2 = utils::read_file(&input_2_name);
 
-  let test_data_1_name = &format!("input/{}/test1.txt", day.name());
-  let input_1_name = &format!("input/{}/part1.txt", day.name());
-  let test_data_2_name = &format!("input/{}/test2.txt", day.name());
-  let input_2_name = &format!("input/{}/part2.txt", day.name());
+  let start_part_1 = std::time::Instant::now();
+  let answer1 = day.solve_part1(&input_data1);
+  println!("{} Part 1: {answer1}", day.name());
+  println!("{:?}", start_part_1.elapsed());
 
-  let test_data1 = utils::read_file(test_data_1_name);
-  let input_data1 = utils::read_file(input_1_name);
+  let start_part_2 = std::time::Instant::now();
+  let answer2 = day.solve_part2(&input_data2);
+  println!("{} Part 2: {answer2}", day.name());
+  println!("{:?}", start_part_2.elapsed());
+}
+
+fn check_day(day: &dyn AOCDay) {
+  println!("Running {}", day.name());
+  let (test_data_1_name, input_1_name, test_data_2_name, input_2_name) = get_filenames(&day.name());
+  let test_data1 = utils::read_file(&test_data_1_name);
+  let input_data1 = utils::read_file(&input_1_name);
+  let test_data2 = utils::read_file(&test_data_2_name);
+  let input_data2 = utils::read_file(&input_2_name);
 
   let test1 = day.solve_part1(&test_data1);
   if test1 == day.test_answer_part1() {
-    println!("Part 1 Test Passed, attemtping to solve");
+    println!("{} Part 1 Test Passed, attemtping to solve", day.name());
     let start_part_1 = std::time::Instant::now();
     let answer1 = day.solve_part1(&input_data1);
-    println!("Part 1: {answer1}");
+    println!("{} Part 1: {answer1}", day.name());
     println!("{:?}", start_part_1.elapsed());
-
-    let test_data_name = if Path::new(test_data_2_name).exists() {
-      test_data_2_name
-    } else {
-      test_data_1_name
-    };
-    let input_data_name = if Path::new(input_2_name).exists() {
-      input_2_name
-    } else {
-      input_1_name
-    };
-
-    let test_data2 = utils::read_file(test_data_name);
-    let input_data2 = utils::read_file(input_data_name);
 
     let test2 = day.solve_part2(&test_data2);
     if test2 == day.test_answer_part2() {
-      println!("Part 2 Test Passed, attemtping to solve");
+      println!("{} Part 2 Test Passed, attemtping to solve", day.name());
       let start_part_2 = std::time::Instant::now();
       let answer2 = day.solve_part2(&input_data2);
-      println!("Part 2: {answer2}");
+      println!("{} Part 2: {answer2}", day.name());
       println!("{:?}", start_part_2.elapsed());
     } else {
-      println!("Part 2 Test Failed");
+      println!("{} Part 2 Test Failed", day.name());
     }
   } else {
-    println!("Part 1 Test Failed");
+    println!("{} Part 1 Test Failed", day.name());
   }
 }
 
 #[allow(clippy::similar_names)]
 fn main() {
+  let args: Vec<String> = env::args().collect();
+  for arg in &args {
+    println!("{arg}");
+  }
+
   let start = std::time::Instant::now();
 
   let day1 = Day1 {};
@@ -140,31 +174,39 @@ fn main() {
   let day24 = Day24 {};
   let day25 = Day25 {};
 
-  run_day(&day1);
-  run_day(&day2);
-  run_day(&day3);
-  run_day(&day4);
-  run_day(&day5);
-  run_day(&day6);
-  run_day(&day7);
-  run_day(&day8);
-  run_day(&day9);
-  run_day(&day10);
-  run_day(&day11);
-  run_day(&day12);
-  run_day(&day13);
-  run_day(&day14);
-  run_day(&day15);
-  run_day(&day16);
-  run_day(&day17);
-  run_day(&day18);
-  run_day(&day19);
-  run_day(&day20);
-  run_day(&day21);
-  run_day(&day22);
-  run_day(&day23);
-  run_day(&day24);
-  run_day(&day25);
+  let days: Vec<Box<dyn AOCDay>> = vec![
+    Box::new(day1),
+    Box::new(day2),
+    Box::new(day3),
+    Box::new(day4),
+    Box::new(day5),
+    Box::new(day6),
+    Box::new(day7),
+    Box::new(day8),
+    Box::new(day9),
+    Box::new(day10),
+    Box::new(day11),
+    Box::new(day12),
+    Box::new(day13),
+    Box::new(day14),
+    Box::new(day15),
+    Box::new(day16),
+    Box::new(day17),
+    Box::new(day18),
+    Box::new(day19),
+    Box::new(day20),
+    Box::new(day21),
+    Box::new(day22),
+    Box::new(day23),
+    Box::new(day24),
+    Box::new(day25),
+  ];
+
+  if args.len() > 1 && args[1] == "run" {
+    days.par_iter().for_each(|day| run_day(&**day));
+  } else {
+    days.par_iter().for_each(|day| check_day(&**day));
+  }
 
   println!("{:?}", start.elapsed());
 }
