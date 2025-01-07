08e337637a48c5d3e19172e01d245d74b728f4c5
Tom Elliott <tom.elliott@faculty.ai>
2025-01-07 10:08:40 +0000

    More sensible approach to Day 20, which saves 10s of minutes

--- src/day20.rs
+++ src/day20.rs
@@ -1,7 +1,7 @@
 use crate::utils::Point;
 use crate::{utils, AOCDay};
 use pathfinding::prelude::astar;
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 const PART_1_EXAMPLE: &str = "5";
 const PART_2_EXAMPLE: &str = "285";
@@ -71,23 +71,7 @@ fn is_test(input: &[Vec<char>]) -> bool {
   input.len() == 15
 }
 
-fn get_neighbours(
-  map: &Map,
-  location: Point,
-  cheat_start: Option<Point>,
-  cheat_end: Option<Point>,
-  cheat_distance: Option<i32>,
-) -> Vec<(Point, i32)> {
-  // If we've ended up on a cheat start, the only neighbour is the end of the cheat_path
-  if let (Some(cheat_start), Some(cheat_end), Some(cheat_distance)) =
-    (cheat_start, cheat_end, cheat_distance)
-  {
-    if location == cheat_start {
-      return vec![(cheat_end, cheat_distance)];
-    }
-  }
-
-  // Otherwise, we are on the track and move as normal
+fn get_neighbours(map: &Map, location: Point) -> Vec<(Point, usize)> {
   let mut neighbours = vec![];
   for (dx, dy) in &[(0, -1), (0, 1), (-1, 0), (1, 0)] {
     let new_pos = (location.x + dx, location.y + dy);
@@ -117,7 +101,7 @@ fn generate_cheat_paths_for_tile(
   map: &Map,
   cheat_start: Point,
   path_length: usize,
-) -> HashSet<(Point, Point, i32)> {
+) -> HashSet<(Point, Point, usize)> {
   // Take all the tiles in the map
   map
     .iter()
@@ -135,18 +119,18 @@ fn generate_cheat_paths_for_tile(
     // get the manhatten distance from the cheat start to the end
     // and filter the ones that are within the path length
     .filter_map(|cheat_end| {
-      let distance = utils::get_manhattan_distance(cheat_start, cheat_end);
-      if distance <= utils::usize_to_i32_x(path_length) {
+      let distance = utils::i32_to_usize_x(utils::get_manhattan_distance(cheat_start, cheat_end));
+      if distance <= path_length {
         Some((cheat_start, cheat_end, distance))
       } else {
         None
       }
     })
-    .collect::<HashSet<(Point, Point, i32)>>()
+    .collect::<HashSet<(Point, Point, usize)>>()
 }
 
-fn get_cheat_paths(map: &Map, path_length: usize) -> HashSet<(Point, Point, i32)> {
-  let track_tiles = map
+fn get_track_tiles(map: &Map) -> Vec<Point> {
+  map
     .iter()
     .enumerate()
     .flat_map(|(row_idx, col)| {
@@ -159,33 +143,7 @@ fn get_cheat_paths(map: &Map, path_length: usize) -> HashSet<(Point, Point, i32)
           y: utils::usize_to_i32_x(row_idx),
         })
     })
-    .collect::<Vec<Point>>();
-
-  let mut result = HashSet::new();
-  for tile in track_tiles {
-    let paths = generate_cheat_paths_for_tile(map, tile, path_length);
-    for (cheat_start, cheat_end, cheat_path_length) in paths {
-      result.insert((cheat_start, cheat_end, cheat_path_length));
-    }
-  }
-
-  result
-}
-
-fn find_shortest_path(
-  map: &Map,
-  start: Point,
-  end: Point,
-  cheat_start: Option<Point>,
-  cheat_end: Option<Point>,
-  cheat_distance: Option<i32>,
-) -> Option<(Vec<Point>, i32)> {
-  astar(
-    &start,
-    |loc| get_neighbours(map, *loc, cheat_start, cheat_end, cheat_distance),
-    |loc| utils::get_manhattan_distance(end, *loc),
-    |loc| *loc == end,
-  )
+    .collect::<Vec<Point>>()
 }
 
 fn count_cheat_paths(
@@ -193,28 +151,73 @@ fn count_cheat_paths(
   start: Point,
   end: Point,
   path_length: usize,
-  required_saving: i32,
+  required_saving: usize,
 ) -> usize {
-  let total_cost = find_shortest_path(map, start, end, None, None, None)
-    .expect("Could not find valid path through maze")
-    .1;
+  let total_cost = astar(
+    &start,
+    |loc| get_neighbours(map, *loc),
+    |loc| utils::i32_to_usize_x(utils::get_manhattan_distance(end, *loc)),
+    |loc| *loc == end,
+  )
+  .expect("Could not find valid path through maze")
+  .1;
 
-  get_cheat_paths(map, path_length)
+  let mut from_start = vec![vec![usize::MAX; map[0].len()]; map.len()];
+
+  let mut from_start_queue: VecDeque<(Point, usize)> = VecDeque::new();
+  from_start_queue.push_back((start, 0));
+  from_start[utils::i32_to_usize_x(start.y)][utils::i32_to_usize_x(start.x)] = 0;
+
+  while !from_start_queue.is_empty() {
+    let (current, cost) = from_start_queue.pop_front().unwrap();
+
+    for (neighbour, neighbour_cost) in get_neighbours(map, current) {
+      let new_cost = cost + neighbour_cost;
+      if new_cost
+        < from_start[utils::i32_to_usize_x(neighbour.y)][utils::i32_to_usize_x(neighbour.x)]
+      {
+        from_start[utils::i32_to_usize_x(neighbour.y)][utils::i32_to_usize_x(neighbour.x)] =
+          new_cost;
+        from_start_queue.push_back((neighbour, new_cost));
+      }
+    }
+  }
+
+  let mut to_end = vec![vec![usize::MAX; map[0].len()]; map.len()];
+  let mut to_end_queue = VecDeque::new();
+  to_end_queue.push_back((end, 0));
+  to_end[utils::i32_to_usize_x(end.y)][utils::i32_to_usize_x(end.x)] = 0;
+
+  while !to_end_queue.is_empty() {
+    let (current, cost) = to_end_queue.pop_front().unwrap();
+
+    for (neighbour, neighbour_cost) in get_neighbours(map, current) {
+      let new_cost = cost + neighbour_cost;
+      if new_cost < to_end[utils::i32_to_usize_x(neighbour.y)][utils::i32_to_usize_x(neighbour.x)] {
+        to_end[utils::i32_to_usize_x(neighbour.y)][utils::i32_to_usize_x(neighbour.x)] = new_cost;
+        to_end_queue.push_back((neighbour, new_cost));
+      }
+    }
+  }
+
+  let all_cheat_paths = get_track_tiles(map)
     .iter()
+    .flat_map(|point| generate_cheat_paths_for_tile(map, *point, path_length))
     .filter_map(|(cheat_start, cheat_end, cheat_distance)| {
-      let result = find_shortest_path(
-        map,
-        start,
-        end,
-        Some(*cheat_start),
-        Some(*cheat_end),
-        Some(*cheat_distance),
-      );
+      let start_cost =
+        from_start[utils::i32_to_usize_x(cheat_start.y)][utils::i32_to_usize_x(cheat_start.x)];
+      let end_cost = to_end[utils::i32_to_usize_x(cheat_end.y)][utils::i32_to_usize_x(cheat_end.x)];
+      let cost = start_cost + cheat_distance + end_cost;
+      let saving = total_cost.saturating_sub(cost);
 
-      result.map(|(_, cost)| (*cheat_start, *cheat_end, cost, total_cost - cost))
+      match saving {
+        _ if saving >= required_saving => Some((cheat_start, cheat_end, saving)),
+        _ => None,
+      }
     })
-    .filter(|(_, _, cost, _)| *cost <= total_cost - required_saving)
-    .count()
+    .collect::<HashSet<(Point, Point, usize)>>();
+
+  all_cheat_paths.len()
 }
 
 pub struct Day20 {}
